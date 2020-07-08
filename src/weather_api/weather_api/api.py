18373d9f4c0b18c2999bf110734bb082ba67aeb3
Steve Kamanke <steve.kamanke@double-eye.com>
2020-07-08 11:03:48 +0200

    weather api

---
+++ src/weather_api/weather_api/api.py
@@ -0,0 +1,21 @@
+import requests
+from django.conf import settings
+from django.http import JsonResponse
+
+
+def weather(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + settings.OPENWEATHER_API_KEY
+    city = request.GET['city']
+
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city': city,
+        'description': r['weather'][0]['description'],
+        'avg_temp': r['main']['temp'],
+        'min_temp': r['main']['temp_min'],
+        'max_temp': r['main']['temp_max'],
+        'humidity': r['main']['humidity'],
+    }
+
+    return JsonResponse(city_weather)
